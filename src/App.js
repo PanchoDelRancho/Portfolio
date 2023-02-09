3295f7e6ae05cacd45dc2b22992761243cb019f0
PanchoDelRancho <faparicio.fa@gmail.com>
2023-02-09 14:05:42 -0600

    borders, ball movement and other features

--- src/App.js
+++ src/App.js
@@ -1,36 +1,210 @@
-import Header from '../src/components/Header';
-import AboutMe from './scenes/AboutMe';
-import { useEffect, useState} from 'react';
+import Header from "../src/components/Header";
+import AboutMe from "./scenes/AboutMe";
+import { useEffect, useState } from "react";
 
+const App = () => {
+  const [size, setSize] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
 
+  useEffect(() => {
+    const handleSize = () => {
+      setSize({ height: window.innerHeight - 80, width: window.innerWidth });
+    };
+    window.addEventListener("resize", handleSize);
 
-
-const App =()=>{
-  const [size, setSize]= useState({height: window.innerHeight,
-    width: window.innerWidth})
-  
-    
-  
-  useEffect(()=>{
-    const handleSize =()=>{
-      setSize({height: window.innerHeight - 80,width: window.innerWidth})
-    }
-    window.addEventListener('resize', handleSize)
-
-    handleSize()
-    
-
-  },[setSize])
-
+    handleSize();
+  }, [setSize]);
 
   return (
     <>
-      <Header />
-      <div style={size}>
-      <AboutMe windowSize = {size}/>
+      <div
+        style={{
+          position: "fixed",
+          width: `${size.width}px`,
+          height: `${size.height + 80}px`,
+        }}
+        className="gameBorder"
+      >
+        <Header />
+        <div style={size}>
+          <AboutMe windowSize={size} />
+        </div>
+      </div>
+
+      <div style={{width: `${size.width}px`}}>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div style={{ postition: "sticky" }}>Hello world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>ss
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
+        <div>hellow world</div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/scenes/AboutMe.js
+++ src/scenes/AboutMe.js
@@ -1,96 +1,147 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useRef, useState, useEffect } from "react";
 
-let paddlePosition = 0
-let ballPosition = 0
-
-
-
-const Paddle = (props) => {
-  const meshRef = useRef();
-  const [yPos, setY] = useState(0);
-  
-
-  useEffect(() => {
-    let speed = .5
-    const handleMove = (e) => {
-      let key = e.key;
-      if(key==='w'){setY(yPos + speed)}
-      if(key==='s'){setY(yPos - speed)}
-      }
-    ;
-    document.addEventListener("keypress", handleMove);
-  }, [yPos]);
-
-  useFrame(() => {
-    if (!meshRef.current) {
-      return;
-    }
-
-    meshRef.current.position.y = yPos;
-    paddlePosition = meshRef.current.position
-    
-  });
-
-  return (
-    <mesh {...props}  ref={meshRef}>
-      <boxGeometry args={[0.3, 2, 0.3]}/>
-      <meshStandardMaterial color="#14FFEC" />
-    </mesh>
-  );
-};
-
-const Ball =(props)=>{
-  const ballRef = useRef()
-  const [acc, setAcc]= useState(false)
-  let ballSpeed = .05
-
-  useFrame(()=>{
-    if (!ballRef.current) {
-      return;
-    }
-    if(acc){
-      ballRef.current.position.x += ballSpeed
-    }
-    if(!acc){
-      ballRef.current.position.x -= ballSpeed
-    }
-    ballPosition = ballRef.current.position
-
-    if (acc === false && ballPosition.x-.4 <= -paddlePosition.x && ballPosition.y <= paddlePosition.y+1 && ballPosition.y >= paddlePosition.y-1){
-      setAcc(!acc)
-    }
-
-    if (acc === true && ballPosition.x+.4 >= paddlePosition.x && ballPosition.y <= paddlePosition.y+1 && ballPosition.y >= paddlePosition.y-1){
-      setAcc(!acc)
-      
-    }
-
-
-  })
-
-  return(
-    <mesh {...props} ref={ballRef}>
-      <sphereGeometry args={[.3, 20, 20]}/>
-      <meshStandardMaterial color='white'/>
-    </mesh>
-  )
-
-}
-
 const AboutMe = ({ windowSize }) => {
   // make the positioning of the boxes responsive
-  const { width } = windowSize;
+  const { width, height } = windowSize;
   let scaleR = [width / 157, 0, -5];
   let scaleL = [-(width / 157), 0, -5];
+
+  // game state variables
+  let paddlePosition = 0;
+  let ballPosition = 0;
+  let gameOn = false;
+  let [lost, setLost] = useState(false);
+  let borders = [
+    -(width / 140.75),
+    width / 140.75,
+    -(height / 151.23),
+    height / 151.23,
+  ];
+
   
 
+  // reset function
+  const reset = () => {
+    console.table(paddlePosition,ballPosition,gameOn,lost)
+    paddlePosition = 0;
+    ballPosition = 0;
+    gameOn = false;
+    setLost(false);
+    
+  };
+
+  if(lost === true){reset()}
+
+
+  const Paddle = (props) => {
+    const meshRef = useRef();
+    const [yPos, setY] = useState(0);
+    
+
+    useEffect(() => {
+      let speed = 0.5;
+      const handleMove = (e) => {
+        if (gameOn === false && lost === false) gameOn = true;
+
+        let key = e.key;
+        if (key === "w") {
+          setY(yPos + speed);
+        }
+        if (key === "s") {
+          setY(yPos - speed);
+        }
+      };
+      document.addEventListener("keypress", handleMove);
+    }, [yPos]);
+
+    useFrame(() => {
+      if (!gameOn) return;
+      if (!meshRef.current) {
+        return;
+      }
+
+      meshRef.current.position.y = yPos;
+      paddlePosition = meshRef.current.position;
+    });
+
+    return (
+      <mesh {...props} ref={meshRef}>
+        <boxGeometry args={[0.3, 2, 0.3]} />
+        <meshStandardMaterial color="#14FFEC" />
+      </mesh>
+    );
+  };
+
+  const Ball = (props) => {
+    const ballRef = useRef();
+    const [acc, setAcc] = useState(false);
+    let ballSpeed = 0.05;
+
+    useFrame(() => {
+      if (!gameOn) {
+       
+      return;
+      }
+
+      if (!ballRef.current) {
+        return;
+      }
+      if (acc) {
+        ballRef.current.position.x += ballSpeed;
+        ballRef.current.position.y += ballSpeed / 3;
+      }
+      if (!acc) {
+        ballRef.current.position.x -= ballSpeed;
+        ballRef.current.position.y -= ballSpeed / 3;
+      }
+      ballPosition = ballRef.current.position;
+      if (
+        ballPosition.x <= borders[0] ||
+        ballPosition.x >= borders[1] ||
+        ballPosition.y <= borders[2] ||
+        ballPosition.y >= borders[3]
+      ) {
+        setLost(true)
+        gameOn = false
+        return ;
+      }
+
+      if (
+        acc === false &&
+        ballPosition.x - 0.4 <= -paddlePosition.x &&
+        ballPosition.y <= paddlePosition.y + 1 &&
+        ballPosition.y >= paddlePosition.y - 1
+      ) {
+        setAcc(!acc);
+      }
+
+      if (
+        acc === true &&
+        ballPosition.x + 0.4 >= paddlePosition.x &&
+        ballPosition.y <= paddlePosition.y + 1 &&
+        ballPosition.y >= paddlePosition.y - 1
+      ) {
+        setAcc(!acc);
+      }
+    });
+
+    return (
+      <mesh {...props} ref={ballRef}>
+        <sphereGeometry args={[0.3, 20, 20]} />
+        <meshStandardMaterial color="white" />
+      </mesh>
+    );
+  };
+
   return (
-    <Canvas style={{ top: 0, position: "absolute" }}>
+    <Canvas
+      style={{ top: 0, position: "absolute", height:window.innerHeight , width: window.innerWidth }}
+    >
       <pointLight position={[0, 0, 6]} />
       <Paddle position={scaleL} />
       <Paddle position={scaleR} />
-      <Ball position={[0,0,-5]}/>
+      <Ball position={[0, 0, -5]} />
     </Canvas>
   );
 };
